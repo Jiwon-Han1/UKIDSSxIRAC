843666baa9d69b7cb5ca1b4818eddff6acf4a100
Jiwon-Han1 <147721921+Jiwon-Han1@users.noreply.github.com>
2024-06-05 17:40:53 +0900

    Update 04. Data Pre-Processing: 0_prep_fits_to_npy.py

--- 04. Data Pre-Processing: 0_prep_fits_to_npy.py
+++ 04. Data Pre-Processing: 0_prep_fits_to_npy.py
@@ -9,6 +9,7 @@ import cv2
 import argparse
 
 # ------- Edit it to recieve variables from the config.py ------- #
+
 root_dir = os.getcwd() # /home/jiwon/UKIDSSxIRAC
 
 survey1 = 'ukidss'
@@ -17,23 +18,31 @@ filters1 = ['J', 'H', 'K']
 survey2 = 'spitzer'
 filters2 = ['ch1', 'ch2', 'ch3', 'ch4']
 
+targetlist = np.loadtxt(os.path.join(root_dir, 'targetlist.txt'), dtype='str')
+
 train_ratio = 0.6
 val_ratio = 0.2
 test_ratio = 0.2
 
+augmentation = ['VF', 'HF', 'Rot90', 'Rot180', 'Rot270', 'VF_Rot90', 'HF_Rot90']
+
+verbose_fits_to_npy = False
+verbose_image_stacking = True
+verbose_augmentation = False
+
 # --------------------------------------------------------------- #
 
+# Load Image List
 data_dir1 = os.path.join(root_dir,'images',survey1)
-# datalist1 = [os.path.splitext(os.path.abspath(x))[0] for x in glob.glob(os.path.join(data_dir1,'*_J.fits'))]
 datalist1 = [os.path.abspath(x) for x in glob.glob(os.path.join(data_dir1,'*.fits'))]
 datalist1.sort()
-# imglist1 = [os.path.basename(x) for x in datalist1]
 
 data_dir2 = os.path.join(root_dir,'images',survey2)
-# datalist2 = [os.path.splitext(os.path.abspath(x))[0] for x in glob.glob(os.path.join(data_dir2,'*_ch1.fits'))]
 datalist2 = [os.path.abspath(x) for x in glob.glob(os.path.join(data_dir2,'*.fits'))]
 datalist2.sort()
-# imglist2 = [os.path.basename(x) for x in datalist2]
+
+print('\n'.join(datalist1[0:3]), '\n')
+print('\n'.join(datalist2[0:4]))
 
 class FitsArrayConverter:
     def __init__(self, filename):
@@ -55,7 +64,8 @@ class FitsArrayConverter:
         savename = root_dir+'/data/'+self.filename.split('images/')[1]
         np.save(os.path.splitext(savename)[0]+pre_processed+'.npy', image)
     
-    def save_image(self, size_fit=True, size_pix=60, upsample=False, up_num=1, marking=False, verbose=False):
+    def save_image(self, size_fit=True, size_pix=60, 
+                   upsample=False, up_num=1, marking=False, verbose=False):
         image = self.get_image()
         org_size = np.shape(image)
 
@@ -69,26 +79,28 @@ class FitsArrayConverter:
         if not marking:
             pre_processed = ''
         if verbose:
-            filter = os.path.splitext(self.filename)[0].split('_')[-1]
-            print(f'{filter}:   {org_size}  ==>  {np.shape(image)}')
+            band = os.path.splitext(self.filename)[0].split('_')[-1]
+            print(f'{band}:   {org_size}  ==>  {np.shape(image)}')
 
         self.save_as_npy(image, pre_processed)
 
+# Make Data Directory
 if not os.path.exists(root_dir+'/data'):
     print(f'\n=== Making "data" Directory ===\n')
     os.makedirs(root_dir+'/data')
     os.makedirs(root_dir+'/data/'+survey1)
     os.makedirs(root_dir+'/data/'+survey2)
-    for i in range(len(filters1)):
-        print(os.path.join(root_dir,'data',survey1,filters1[i]))
-        # os.makedirs(os.path.join(root_dir,'data',survey1,filters1[i]))    
+    # for i in range(len(filters1)):
+    #     print(os.path.join(root_dir,'data',survey1,filters1[i]))
+        # os.makedirs(os.path.join(root_dir,'data',survey1,filters1[i])) 
 
+# Convert the Images into Numpy Array
 print('\n###--- Image Pre-processing: UKIDSS ---###')
 for i in range(len(datalist1)): 
-    verbose = False     ### Check!
+    verbose = verbose_fits_to_npy     ### Check!
     if verbose:     
         if i%len(filters1) == 0:
-            print('\n'+'[[ # '+os.path.basename(datalist1[i]).split('_')[-2]+' ]]')
+            print('\n'+'[[ # '+os.path.basename(datalist1[i]).split('_')[-2]+' ]]') # to print target name
     imgarray1 = FitsArrayConverter(datalist1[i])
     imgarray1.save_image(size_fit=True, size_pix=300, upsample=False, up_num=1, verbose=verbose)
 
@@ -97,10 +109,10 @@ print()
 print('\n###---  Image Pre-processing: IRAC  ---###')
 
 for i in range(len(datalist2)): 
-    verbose = False     ### Check!
+    verbose = verbose_fits_to_npy     ### Check!
     if verbose: 
         if i%len(filters2) == 0:
-            print('\n'+'[[ # '+os.path.basename(datalist2[i]).split('_')[-2][1:]+' ]]')
+            print('\n'+'[[ # '+os.path.basename(datalist2[i]).split('_')[-2][1:]+' ]]') # to print target name
 
     imgarray2 = FitsArrayConverter(datalist2[i])
     imgarray2.save_image(size_fit=True, size_pix=60, upsample=True, up_num=5, verbose=verbose)
