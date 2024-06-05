c3d21c93697a66ba7097c910014184c0b10dfd43
Jiwon-Han1 <147721921+Jiwon-Han1@users.noreply.github.com>
2024-06-05 17:36:40 +0900

    Update 05. Data Pre-Processing: 1_prep_stacking_and_augmentation.py

--- 05. Data Pre-Processing: 1_prep_stacking_and_augmentation.py
+++ 05. Data Pre-Processing: 1_prep_stacking_and_augmentation.py
@@ -1,4 +1,5 @@
 ### Saved as ~/UKIDSSxIRAC/1_prep_stacking_and_augmentation.py
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
@@ -28,20 +29,24 @@ filters1 = ['J', 'H', 'K']
 survey2 = 'spitzer'
 filters2 = ['ch1', 'ch2', 'ch3', 'ch4']
 
+targetlist = np.loadtxt(os.path.join(root_dir, 'targetlist.txt'), dtype='str')
+
 train_ratio = 0.6
 val_ratio = 0.2
 test_ratio = 0.2
 
 augmentation = ['VF', 'HF', 'Rot90', 'Rot180', 'Rot270', 'VF_Rot90', 'HF_Rot90']
-augmentation = []
+
+verbose_fits_to_npy = False
+verbose_image_stacking = True
+verbose_augmentation = False
 
 # --------------------------------------------------------------- #
-
-targetlist = np.loadtxt(os.path.join(root_dir, 'targetlist.txt'), dtype='str')
-
 if not os.path.exists(root_dir+'/data/merged'):
     print(f'\n=== Making "merged" Directory ===\n')
     os.makedirs(root_dir+'/data/merged')
+
+save_dir = root_dir+'/data/merged'
 
 class ImageStacker:
     def __init__(self, save_dir, channels_order=None):
@@ -70,57 +75,45 @@ class ImageStacker:
         np.save(os.path.join(self.save_dir,f'{target}_X.npy'), X_data)
         np.save(os.path.join(self.save_dir,f'{target}_y.npy'), y_data)
 
-    # def save_npy(self, target, aug_type):
-    #     np.save(os.path.join(self.save_dir,) )
-    #     pass
-
-    # def augmentation(self, org_data, target, augmentations):
-    #     pass
-
-
-class ImageAugmentation:
+class ImageAugmentation: # To call getattr
     def __init__(self):
         pass
 
-    def vertical_flip(self, image_array):
+    def VF(self, image_array): # vertical_flip
         return np.flipud(image_array)
 
-    def horizontal_flip(self, image_array):
+    def HF(self, image_array): # horizontal_flip
         return np.fliplr(image_array)
 
-    def rotate_90(self, image_array):
+    def Rot90(self, image_array): # rotate_90
         # return np.rot90(image_array, k=1, axes=(image_array.ndim-2, image_array.ndim-1))
         return np.rot90(image_array, k=1, axes=(0, 1))
 
-    def rotate_180(self, image_array):
+    def Rot180(self, image_array): # rotate_180
         # return np.rot90(image_array, k=2, axes=(image_array.ndim-2, image_array.ndim-1))
         return np.rot90(image_array, k=2, axes=(0, 1))
     
-    def rotate_270(self, image_array):
+    def Rot270(self, image_array): # rotate_270
         # return np.rot90(image_array, k=3, axes=(image_array.ndim-2, image_array.ndim-1))
         return np.rot90(image_array, k=3, axes=(0, 1))
     
-    def rotate_90_flipped_VF(self, image_array):
+    def VF_Rot90(self, image_array): # rotate_90_flipped_VF
         return np.rot90(np.flipud(image_array), k=1, axes=(0, 1)) 
 
-    def rotate_90_flipped_HF(self, image_array):
+    def HF_Rot90(self, image_array): # rotate_90_flipped_HF
         return np.rot90(np.fliplr(image_array), k=1, axes=(0, 1)) 
-    
-    def aug_apply(self, target, data, save_dir, var, augmentations):
-        if 'VF' in augmentation:
-            np.save(os.path.join(save_dir,target,'_VF',f'_{var}.npy'), self.vertical_flip(data))
     
     def augment_and_visualize(self, image_array):
         # Perform augmentations
         augments = {
             'Original': image_array,
-            'Vertical Flip': self.vertical_flip(image_array),
-            'Horizontal Flip': self.horizontal_flip(image_array),
-            'Rotate 90': self.rotate_90(image_array),
-            'Rotate 180': self.rotate_180(image_array),
-            'Rotate 270': self.rotate_270(image_array),
-            'Vertical Flip + Rotate 90': self.rotate_90_flipped_VF(image_array),
-            'Horizontal Flip + Rotate 90': self.rotate_90_flipped_HF(image_array)
+            'Vertical Flip': self.VF(image_array),
+            'Horizontal Flip': self.HF(image_array),
+            'Rotate 90': self.Rot90(image_array),
+            'Rotate 180': self.Rot180(image_array),
+            'Rotate 270': self.Rot270(image_array),
+            'Vertical Flip + Rotate 90': self.VF_Rot90(image_array),
+            'Horizontal Flip + Rotate 90': self.HF_Rot90(image_array)
         }
 
         # Visualize the results
@@ -130,54 +123,38 @@ class ImageAugmentation:
             plt.imshow(aug_image)
             plt.title(title)
             plt.axis('off')
-        
         plt.tight_layout()
         plt.show()
 
-save_dir = root_dir+'/data/merged'
 img_stacker = ImageStacker(save_dir)
-# img_stacker = ImageStacker(save_dir+'/3_channels', channels_order=['H','K','ch2'])
+augmentor = ImageAugmentation()
+
+# if not os.path.exists(root_dir+'/data_3_channels'):
+#     print(f'\n=== Making "3-channel merged" Directory ===\n')
+#     os.makedirs(root_dir+'/data_3_channels')
+# img_stacker = ImageStacker(root_dir+'/data_3_channels', channels_order=['H','K','ch2'])
+
+img_stacker = ImageStacker(save_dir)
+augmentor = ImageAugmentation()
 
 for target in targetlist:
     target_datalist = [os.path.abspath(x) for x in glob.glob(os.path.join(root_dir,'data','**', f'*{target}*'), recursive=True)]
-    img_stacker.save_as_Xy(target, target_datalist, verbose=True)
+    img_stacker.save_as_Xy(target, target_datalist, verbose=verbose_image_stacking)
 
     if len(augmentation) > 0:
-        augmentor = ImageAugmentation()
-
         X_data = np.load(os.path.join(save_dir,f'{target}_X.npy'))
         y_data = np.load(os.path.join(save_dir,f'{target}_y.npy'))
 
-        if 'VF' in augmentation:
-            print(os.path.join(save_dir,target,'_VF','_X.npy'))
-            print(os.path.join(save_dir,target,'_VF','_y.npy'))
-            # np.save(os.path.join(save_dir,target,'_VF','_X.npy'),augmentor.vertical_flip(X_data))
-            # np.save(os.path.join(save_dir,target,'_VF','_y.npy'),augmentor.vertical_flip(y_data))
-
-        if 'HF' in augmentation:
-            print(os.path.join(save_dir,target,'_HF','_X.npy'))
-            print(os.path.join(save_dir,target,'_HF','_y.npy'))           
-            # np.save(os.path.join(save_dir,target,'_HF','_X.npy'),augmentor.horizontal_flip(X_data))
-            # np.save(os.path.join(save_dir,target,'_HF','_y.npy'),augmentor.horizontal_flip(y_data))
-
-        if 'Rot90' in augmentation:
-            print(os.path.join(save_dir,target,'_Rot90','_X.npy'))
-            print(os.path.join(save_dir,target,'_Rot90','_y.npy'))
-            # np.save(os.path.join(save_dir,target,'_Rot90','_X.npy'),augmentor.rotate_90(X_data))
-            # np.save(os.path.join(save_dir,target,'_Rot90','_y.npy'),augmentor.rotate_90(y_data))
-
-        if 'Rot180' in augmentation:
-            print(os.path.join(save_dir,target,'_Rot180','_X.npy'))
-            print(os.path.join(save_dir,target,'_Rot180','_y.npy'))
-            # np.save(os.path.join(save_dir,target,'_Rot180','_X.npy'),augmentor.rotate_180(X_data))
-            # np.save(os.path.join(save_dir,target,'_Rot180','_y.npy'),augmentor.rotate_180(y_data))
-
-        if 'Rot270' in augmentation:
-            print(os.path.join(save_dir,target,'_Rot270','_X.npy'))
-            print(os.path.join(save_dir,target,'_Rot270','_y.npy'))
-            # np.save(os.path.join(save_dir,target,'_Rot270','_X.npy'),augmentor.rotate_270(X_data))
-            # np.save(os.path.join(save_dir,target,'_Rot270','_y.npy'),augmentor.rotate_270(y_data))
-
+        for aug in augmentation:
+            aug_data_X = getattr(augmentor, aug)(X_data)
+            aug_data_y = getattr(augmentor, aug)(y_data)
+            
+            np.save(os.path.join(save_dir, f"{target}_{aug}_X.npy"), aug_data_X)
+            np.save(os.path.join(save_dir, f"{target}_{aug}_y.npy"), aug_data_y)
+            
+            if verbose_augmentation:
+                print(os.path.join(save_dir,f"{target}_{aug}_X.npy"))
+                print(os.path.join(save_dir,f"{target}_{aug}_y.npy"))
 
 ### Example (Dog)
 image_path = os.path.join(root_dir,'dog.png')
